b23981012ba1b31a82e2d0cbf099013e33acafd2
Matt Horn <mathphreak@gmail.com>
2016-08-24 00:29:41 -0500

    Watch for changes to subject executable

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const child = require('child_process');
 const fs = require('fs');
 const concat = require('concat-stream');
 const merge = require('merge-stream');
+const _ = require('lodash');
+
+let runningProcesses = [];
+let subjectWatcher;
+
+function updateForms() {
+  if (runningProcesses.length > 0 || subjectWatcher !== undefined) {
+    document.getElementById('run').innerText = 'Stop!';
+  } else {
+    document.getElementById('run').innerText = 'Run!';
+  }
+
+  Array.from(document.querySelectorAll('input')).forEach(e => {
+    e.disabled = (subjectWatcher !== undefined);
+  });
+}
 
 function run(exePath, inputFile, handleOutput) {
   function finished(result) {
@@ -10,9 +26,15 @@ function run(exePath, inputFile, handleOutput) {
   const output = concat(finished);
   const input = fs.createReadStream(inputFile);
   const process = child.spawn(exePath);
+  runningProcesses.push(process);
   input.pipe(process.stdin);
   merge(process.stdout, process.stderr).pipe(output);
   process.on('error', err => console.log(err));
+  process.on('exit', () => {
+    runningProcesses = runningProcesses.filter(p => p !== process);
+    updateForms();
+  });
+  updateForms();
 }
 
 function compare(subjectPath, truthPath, testCase) {
@@ -44,7 +66,8 @@ function compare(subjectPath, truthPath, testCase) {
   }
 }
 
-document.getElementById('run').addEventListener('click', () => {
+function runTest() {
+  runningProcesses.forEach(p => p.kill());
   const subjectPath = document.querySelector('input[name="subject"]').files[0].path;
   const truthPath = document.querySelector('input[name="truth"]').files[0].path;
   const testCases = Array.from(document.querySelectorAll('input[name="testcase"]')).map(x => Array.from(x.files)).reduce((a, b) => a.concat(b)).map(x => x.path);
@@ -54,5 +77,22 @@ document.getElementById('run').addEventListener('click', () => {
   });
   for (const tcPath of testCases) {
     compare(subjectPath, truthPath, tcPath);
+  }
+}
+
+document.getElementById('run').addEventListener('click', () => {
+  if (document.getElementById('run').innerText === 'Run!') {
+    if (document.querySelector('input[name="watch-subject"]').checked) {
+      const subjectPath = document.querySelector('input[name="subject"]').files[0].path;
+      subjectWatcher = fs.watch(subjectPath, _.debounce(runTest, 2000, {trailing: true}));
+    }
+    runTest();
+  } else {
+    runningProcesses.forEach(p => p.kill());
+    if (subjectWatcher !== undefined) {
+      subjectWatcher.close();
+      subjectWatcher = undefined;
+    }
+    updateForms();
   }
 });
