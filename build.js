24f78c84f843995990494aadf5fef528102af38b
Matt Horn <mathphreak@gmail.com>
2016-08-24 15:30:59 -0500

    Build executables on Travis

---
+++ build.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const shell = require('shelljs');
+const archiver = require('archiver');
+
+const version = require('./package.json').version;
+
+shell.cd('dist/');
+
+const systems = [];
+
+shell.ls().forEach(folder => {
+  if (folder.startsWith('Verbatim')) {
+    const [, platform, arch] = folder.split('-');
+    const id = `${platform}-${arch}`;
+    systems.push(id);
+    shell.mkdir(id);
+    shell.mv(folder, `${id}/Verbatim v${version}`);
+  }
+});
+
+shell.cd('..');
+
+shell.mkdir('build');
+
+systems.forEach(id => {
+  if (id === 'darwin-x64') {
+    shell.pushd('dist/darwin-x64');
+    shell.exec(`tar czf ../../build/Verbatim-v${version}-${id}.tar.gz *`);
+    console.log(`Built for ${id}`);
+    shell.popd();
+  } else {
+    const archive = archiver.create('zip');
+    const out = fs.createWriteStream(`build/Verbatim-v${version}-${id}.zip`);
+    archive.on('end', () => console.log(`Built for ${id}`));
+    archive.directory(`dist/${id}/`, '/');
+    archive.finalize();
+    archive.pipe(out);
+  }
+});
