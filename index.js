261ae81d7f389f2089465bca3ec643a138dd3902
Matt Horn <mathphreak@gmail.com>
2016-08-26 15:59:48 -0500

    Make test cases smarter - fix #1

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const child = require('child_process');
 const fs = require('fs');
-const path = require('path');
 
 const concat = require('concat-stream');
 const merge = require('merge-stream');
@@ -10,10 +9,92 @@ const _ = require('lodash');
 let runningProcesses = [];
 let subjectWatcher;
 
+function buildCaseLabel(context, name, text) {
+  const enable = document.createElement('input');
+  enable.type = 'checkbox';
+  enable.name = name + '-enable';
+  enable.addEventListener('change', () => {
+    context.querySelector(`input[name="${name}"]`).disabled = !enable.checked;
+    context.querySelector(`input[name="${name}"]`).dataset.disabled = enable.checked ? '' : true;
+  });
+  const label = document.createElement('label');
+  label.appendChild(enable);
+  label.appendChild(document.createTextNode(' ' + text));
+  return label;
+}
+
+function buildEmptyCase() {
+  const result = document.createElement('div');
+  result.className = 'case';
+
+  const name = document.createElement('input');
+  name.type = 'text';
+  name.name = 'name';
+  const nameP = document.createElement('p');
+  nameP.className = 'name';
+  nameP.appendChild(name);
+
+  const workdirLabel = buildCaseLabel(result, 'workdir', 'Working directory:');
+  const workdir = document.createElement('input');
+  workdir.disabled = true;
+  workdir.dataset.disabled = true;
+  workdir.type = 'file';
+  workdir.name = 'workdir';
+  workdir.webkitdirectory = true;
+  const workdirP = document.createElement('p');
+  workdirP.className = 'workdir';
+  workdirP.appendChild(workdirLabel);
+  workdirP.appendChild(workdir);
+
+  const argsLabel = buildCaseLabel(result, 'args', 'Command line arguments:');
+  const args = document.createElement('input');
+  args.disabled = true;
+  args.dataset.disabled = true;
+  args.type = 'text';
+  args.name = 'args';
+  const argsP = document.createElement('p');
+  argsP.className = 'args';
+  argsP.appendChild(argsLabel);
+  argsP.appendChild(args);
+
+  const stdinLabel = buildCaseLabel(result, 'stdin', 'Console input file:');
+  const stdin = document.createElement('input');
+  stdin.disabled = true;
+  stdin.dataset.disabled = true;
+  stdin.type = 'file';
+  stdin.name = 'stdin';
+  const stdinP = document.createElement('p');
+  stdinP.className = 'stdin';
+  stdinP.appendChild(stdinLabel);
+  stdinP.appendChild(stdin);
+
+  result.appendChild(nameP);
+  result.appendChild(workdirP);
+  result.appendChild(argsP);
+  result.appendChild(stdinP);
+
+  const resultWrapper = document.createElement('div');
+  resultWrapper.className = 'case-wrapper';
+
+  const removeButton = document.createElement('button');
+  removeButton.type = 'button';
+  removeButton.innerText = '\u2718';
+  removeButton.addEventListener('click', evt => {
+    resultWrapper.remove();
+    evt.preventDefault();
+  });
+
+  resultWrapper.appendChild(result);
+  resultWrapper.appendChild(removeButton);
+
+  return resultWrapper;
+}
+
 function handleError(err) {
   document.querySelector('#error').hidden = false;
   document.querySelector('#error .name').innerText = err.name;
-  document.querySelector('#error .message').innerText = err.message;
+  document.querySelector('#error .stack').innerText = err.stack;
+  console.error(err);
 }
 
 process.on('uncaughtException', handleError);
@@ -28,19 +109,21 @@ function updateForms() {
   }
 
   Array.from(document.querySelectorAll('input')).forEach(e => {
-    e.disabled = (subjectWatcher !== undefined);
+    e.disabled = (subjectWatcher !== undefined) || e.dataset.disabled;
   });
 }
 
-function run(exePath, inputFile, options, handleOutput) {
+function run(exePath, options, handleOutput) {
   function finished(result) {
     handleOutput(result.toString('utf8'));
   }
   const output = concat(finished);
-  const input = fs.createReadStream(inputFile);
-  const process = child.spawn(exePath, options.args, {cwd: options.cwd});
+  const process = child.spawn(exePath, options.args, {cwd: options.workdir});
   runningProcesses.push(process);
-  input.pipe(process.stdin);
+  if (options.stdin) {
+    const input = fs.createReadStream(options.stdin);
+    input.pipe(process.stdin);
+  }
   merge(process.stdout, process.stderr).pipe(output);
   process.on('error', handleError);
   process.on('exit', () => {
@@ -69,7 +152,7 @@ function buildResults(results, testCase) {
   row.appendChild(buildResultNode(results.subject, className, 'exe-output subject'));
   row.appendChild(buildResultNode(results.truth, className, 'exe-output truth'));
   const label = document.createElement('p');
-  label.innerText = path.parse(testCase).base;
+  label.innerText = testCase.name || JSON.stringify(testCase);
   const result = document.createElement('div');
   result.className = 'exe-output';
   result.appendChild(label);
@@ -77,13 +160,33 @@ function buildResults(results, testCase) {
   return result;
 }
 
-function compare(options, testCase) {
+function parseCase(caseDiv) {
+  const result = {};
+  function input(name) {
+    return caseDiv.querySelector(`input[name="${name}"]`);
+  }
+  if (input('workdir-enable').checked && input('workdir').files[0]) {
+    result.workdir = input('workdir').files[0].path;
+  }
+  if (input('args-enable').checked) {
+    result.args = shellQuote.parse(input('args').value);
+  }
+  if (input('stdin-enable').checked && input('stdin').files[0]) {
+    result.stdin = input('stdin').files[0].path;
+  }
+  if (input('name').value.length > 0) {
+    result.name = input('name').value;
+  }
+  return result;
+}
+
+function compare(subjectPath, truthPath, testCase) {
   const results = {};
-  run(options.subjectPath, testCase, options, out => {
+  run(subjectPath, testCase, out => {
     results.subject = out;
     handleResults();
   });
-  run(options.truthPath, testCase, options, out => {
+  run(truthPath, testCase, out => {
     results.truth = out;
     handleResults();
   });
@@ -99,23 +202,11 @@ function runTest() {
   runningProcesses.forEach(p => p.kill());
   const subjectPath = document.querySelector('input[name="subject"]').files[0].path;
   const truthPath = document.querySelector('input[name="truth"]').files[0].path;
-  const testCases = Array.from(document.querySelectorAll('input[name="testcase"]')).map(x => Array.from(x.files)).reduce((a, b) => a.concat(b)).map(x => x.path);
-
-  const cwdFile = document.querySelector('input[name="cwd"]').files[0];
-  const cwdPath = cwdFile === undefined ? undefined : cwdFile.path;
-
-  const cliArgs = document.querySelector('input[name="args"]').value;
-
-  const options = {
-    subjectPath,
-    truthPath,
-    cwd: cwdPath,
-    args: shellQuote.parse(cliArgs)
-  };
+  const testCases = Array.from(document.querySelectorAll('.case')).map(parseCase);
 
   document.getElementById('output').innerHTML = '';
-  for (const tcPath of testCases) {
-    compare(options, tcPath);
+  for (const testCase of testCases) {
+    compare(subjectPath, truthPath, testCase);
   }
 }
 
@@ -135,3 +226,9 @@ document.getElementById('run').addEventListener('click', () => {
     updateForms();
   }
 });
+
+document.getElementById('new-case').addEventListener('click', () => {
+  document.querySelector('#cases').appendChild(buildEmptyCase());
+});
+
+document.querySelector('#cases').appendChild(buildEmptyCase());
